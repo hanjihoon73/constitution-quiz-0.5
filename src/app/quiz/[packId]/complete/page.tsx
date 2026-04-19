9b2587421126f857c35fe37ba3c1ac9e39d43403
jason-quebon <jason@quebon.com>
2026-04-19 16:54:59 +0900

    trigger: 버셀 재배포

--- src/app/quiz/[packId]/complete/page.tsx
+++ src/app/quiz/[packId]/complete/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-// deploy trigger
+// deploy trigger retry
 import { useEffect, useState, useCallback } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { MobileFrame } from '@/components/common';
